27525d5ec9edb1ef7fe4d5cb2dc7546610f9ccdf
SadCyberTeam <45815442+SADCyberTeam@users.noreply.github.com>
2019-01-02 16:39:48 +0700

    Create Str.sh

---
+++ Str.sh
@@ -0,0 +1,139 @@
+ #!/bin/bash
+
+#version 0.1
+
+#ImJonedHidup:v
+#ScSpamToMantan:v
+#SpamerGblk:v
+#RecodeOrCopyPaste?
+#GaGunaGoblok!! 
+
+
+# Variables
+
+b='\033[1m'
+
+u='\033[4m'
+
+bl='\E[30m'
+
+r='\E[31m'
+
+g='\E[32m'
+
+bu='\E[34m'
+
+m='\E[35m'
+
+c='\E[36m'
+
+w='\E[37m'
+
+endc='\E[0m'
+
+enda='\033[0m'
+
+blue='\e[1;34m'
+
+cyan='\e[1;36m'
+
+red='\e[1;31m'
+
+clear
+
+figlet -f bigmono9 "Spamer " | lolcat
+
+echo "\033[38;1m ------------------------------------------------------------------"| lolcat
+
+echo "\033[31;1m " "My Contact : 0895422939836 "
+
+echo "\033[32;1m" " From : S.A.D Cyber Team "
+
+echo "\033[37;1m" " Date : Wednesday 02 - 01 - 2019 "
+
+echo "\033[36;1m" " By Tn. Star :v "
+
+echo "\033[35;1m" " #Budidayakan Spam Mantan#"
+
+echo "\033[38;1m" " ------------------------------------------------------------------"| lolcat
+
+echo ""
+
+     echo $b " SPAM SMS To Mantan >:'v "
+
+    
+
+     echo $b " (01)TELKOMSEL "
+
+     echo $b " (02)PHD"
+
+     echo $b " (03)TOKOPEDIA" 
+
+     echo $b " (99)Keluar "
+
+echo $b " pilih Senjata lu cuk!  :v"
+
+read -p "  ~~#  "  ezz; 
+
+#Telkomsel
+
+case $ezz in
+
+
+if [ $ezz = 01 ] || [ $ezz = 1]
+
+then
+
+clear
+
+toilet -f lean "TELKOMSEL"
+
+php telkomsel.php
+
+sh Spamer.sh
+
+fi
+
+
+if [ $ezz = 02 ] || [ $ezz = 2 ]
+
+then
+
+clear 
+
+toilet -f lean "PHD"
+
+php phd.php
+
+sh Spamer.sh
+
+fi
+
+if [ $ezz = 03 ] || [ $ezz = 3 ]
+
+then
+
+clear
+
+toilet -f lean "TOKPED"
+
+php tokped.php
+
+sh Spamer.sh
+
+fi
+
+
+if [ $ezz = 99 ]
+
+then
+
+echo "semoga Mantan Mu senang gan :v   "
+
+echo ""
+
+echo " THX Ferguoso :v "
+
+fi
+
+
