dede261e2e71beb76b9dd3377ace8c94a13e4fb9
SadCyberTeam <45815442+SADCyberTeam@users.noreply.github.com>
2019-01-03 13:52:23 +0700

    Update Grab.py

--- Grab.py
+++ Grab.py
@@ -1,33 +1,21 @@
- #!/usr/bin/python
+#!/usr/bin/python
 
-
-# - Spammer Gblk:v
-
-# | Description: I'm a handsome Boy :v
-
-# | Author: Star Tamvan :v
-
-# | Date: 02/01/2019
-
-# | Disclaimer: belajar Dari Jalanan ga dari guru!!! 
-
+# - Spammer v3.0
+# | Description: spams a phone number by sending it a lot of sms by using Grab API
+# | Moderator : Tn Star 
+# | Date: 03/01/2019
+# | What's New?
+# | - Added coloring
+# | - Added support multiple proxies (BETA)
+# | - Added feature to create a new separate thread for each proxy in order to increase spam speed (BETA)
+# | - Added support for phone number with +xxxxxx format 
+# | - And another fixes
 
 import spammer_class
-
 spammer = spammer_class.Spammer()
-
-spammer.author = "Star Handsome :v "
-
+spammer.author = "TuanStar"
 try:
-
-    spammer.main()
-
+    spammer.main()
 except KeyboardInterrupt:
-
-    print spammer_class.color.FAIL+spammer_class.color.REVERSE+"\r[!][except] KeyboardInterrupt detected! Exiting . . ."+spammer_class.color.ENDC
-
-    exit()
-
-
-
-
+    print spammer_class.color.FAIL+spammer_class.color.REVERSE+"\r[!][except] KeyboardInterrupt detected! Exiting . . ."+spammer_class.color.ENDC
+    exit()
